8bb7d3dda04b75a0741a675564866ed609a06c02
pny284 <hifzaawan1995@gmail.com>
2024-07-03 17:09:47 +0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h1>This is testing file
+
+     </h1>
+     <button>Register</button>
+    </div>
+  );
+}
+
+export default App;
